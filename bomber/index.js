ae8024e5346d817b3897aacc0223eed76e532221
Windows 20 <kasiak0101@gmail.com>
2023-10-02 19:38:33 +0200

    Website v2

---
+++ bomber/index.js
@@ -0,0 +1,18 @@
+const discord = document.getElementById("discord")
+const reddit = document.getElementById("reddit")
+const tiktok = document.getElementById("tiktok")
+const twitch = document.getElementById("twitch")
+const github = document.getElementById("github")
+const minecraft = document.getElementById("minecraft")
+
+discord.addEventListener("click", () => location.href = "https://discord.com/users/952182705934696488")
+
+reddit.addEventListener("click", () => location.href = "https://reddit.com/user/windows20_real_")
+
+tiktok.addEventListener("click", () => location.href = "https://tiktok.com/@w20mc1")
+
+twitch.addEventListener("click", () => location.href = "https://twitch.tv/w20mc")
+
+github.addEventListener("click", () => location.href = "https://github.com/win20-official")
+
+minecraft.addEventListener("click", () => location.href = "https://namemc.com/profile/W20MC.1")
